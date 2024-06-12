d5a56734c0b73e83e4b2d20ec4d2d7b237cd064a
open.grieves@gmail.com <sudo@redhat.com>
2024-06-12 13:39:24 +0200

    fix of script, re-sign

--- scripts/ansible-isolated.sh
+++ scripts/ansible-isolated.sh
@@ -5,13 +5,16 @@ ANSIBLE_DIR=/root/ansible-isolated
 
 ansible-sign project gpg-verify $ANSIBLE_DIR
 if [ "$?" -eq 0 ]; then
-	logger -t "$0 validated playbook and inventory"
+	logger -t "ansible-isolated" "OK: validated playbook and inventory"
 	ansible-playbook -i /root/ansible-isolated/inventory /root/ansible-isolated/playbooks/ping.yml
-	logger -t "$0 successfully ran playbook"
+	if [ "$?" -eq 0 ]; then
+		logger -t "ansible-isolated" "OK: successfully ran playbook"
+	else
+		logger -t "ansible-isolated" "WARNING: failed to run playbook"
+	fi
 	
 else
-	logger -t "$0 WARNING: failed to validate content."
+	logger -t "ansible-isolated" "CRITICAL: failed to validate content."
 	echo "Error: Cannot validate inventory / playbook."
 	exit 1
 fi
-
